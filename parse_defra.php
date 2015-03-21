10c2b0cafeb33752384023f8b47afa8f3cc129fd
Sebastian Mellor (N7010) <sebble@sebble.com>
2015-03-21 15:50:34 +0000

    defra parser

---
+++ parse_defra.php
@@ -0,0 +1,34 @@
+<?php
+
+function fetch_defra($site_id = 'ACTH', $view = 'last_hour') {
+
+    $site_id = preg_replace('![^A-Z]!','',$site_id);
+    $view = preg_replace('![^a-z_]!','',$view);
+    $url = "http://uk-air.defra.gov.uk/data/site-data?f_site_id=${site_id}&view=${view}";
+    
+    $html = file_get_contents($url);
+    $html_without_attributes = preg_replace('!<([a-z0-9]+) [^/>]+>!','<$1>',$html);
+    preg_match('!<table>.+?</table>!s',$html_without_attributes,$m);
+    $html_table_clean = preg_replace('!</td>\r\n!','</td>',$m[0]);
+    $html_table_clean = preg_replace('!<tr>\r\n!','<tr>',$html_table_clean);
+    $html_table_clean = preg_replace('!\t!','',$html_table_clean);
+    $html_table_clean = preg_replace('!\r!','',$html_table_clean);
+    $html_table_clean = preg_replace('!\n\n!',"\n",$html_table_clean);
+    
+    preg_match_all('!<th>(.+?)</th>!',$html_table_clean,$m);
+    $headings = $m[1];
+    
+    preg_match_all('!<tr>(.+?)</tr>!',$html_table_clean,$m);
+    $data = array();
+    foreach ($m[1] as $v) {
+        preg_match_all('!<td>(.+?)</td>!',$v,$n);
+        $data[] = $n[1];
+    }
+    
+    return array(
+        'headings'=> $headings,
+        'data'=> $data
+    );
+}
+
+print_r(fetch_defra('ACTH','last_hour'));
