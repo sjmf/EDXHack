e62fa5be23a7e20ad52899b416a606c7b10931b7
Matt <matthew.mrshll@gmail.com>
2015-03-21 20:30:52 +0000

    Begin noise parsing function

--- web/index.php
+++ web/index.php
@@ -17,8 +17,10 @@ $app->register(new Silex\Provider\TwigServiceProvider(), array('twig.path' => __
 
 $app->get('/', function() use($app){
 
-  return $app['twig']->render('index.twig', array());
+  // return $app['twig']->render('index.twig', array());
+  getNoisePollution(678, 678);
 
+  return 'Hello';
   // return getAirPollution('567', '5678');
   // return 'end';
 });
@@ -44,12 +46,22 @@ function getAirPollution($lat, $long)
 
 }
 // =================================================================
-//  Get the Noise Pollution
+//  Get the Noise Pollution based off of lat long data
 // =================================================================
 function getNoisePollution($lat, $long)
 {
+
   // Get the Noise pollution data from online and convert it to JSON
   $data = array_map('str_getcsv', file('http://data.defra.gov.uk/env/strategic_noise_mapping/r2_strategic_noise_mapping.csv'));
+
+  // Fill this with the keys passed down from Sam
+  $locationMap = array();
+
+  // Get Location from the location map
+  
+
+
+  print_r($data[0]);
 
   return json_encode($data);
 }
