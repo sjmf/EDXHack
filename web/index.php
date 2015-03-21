757d4e725fa3fc7f967766e5aabdfd7f61c3fa0a
Matt <matthew.mrshll@gmail.com>
2015-03-21 15:19:32 +0000

    Stuff is happeninggit statusgit status OMG!

--- web/index.php
+++ web/index.php
@@ -20,8 +20,29 @@ $app->post('/getPollution', function(Request $request) use($app, $DEFRA_TOON){
 });
 
 $app->get('/', function() use ($DEFRA_TOON){
-  return $DEFRA_TOON;
+
+  return getAirPollution('567', '5678');
 });
+
+// =================================================================
+//  Get the Air Pollution Levels based off of Lat / Long Data
+// =================================================================
+function getAirPollution($lat, $long)
+{
+  $feed = implode(file('http://uk-air.defra.gov.uk/assets/rss/current_site_levels.xml'));
+  $xml = simplexml_load_string($feed);
+  $json = json_encode($xml);
+
+  return $json;
+
+}
+// =================================================================
+//  Get the Noise Pollution
+// =================================================================
+function getNoisePollution($lat, $long)
+{
+  
+}
 
 $app->run()
 
